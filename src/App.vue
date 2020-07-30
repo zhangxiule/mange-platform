982761277c7194c7d684023c12583f4fec496ff8
zhangxiule <zhangxiule@126.com>
2020-07-30 21:31:31 +0800

    js

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
+// <script src='../static/js/iconfont.js'></script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import "../static/css/main.css";
+    @import "../static/css/color-dark.css";     /*深色主题*/
+    /*@import "../static/css/theme-green/color-green.css";   浅绿色主题*/
 </style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,33 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'  //默认主题
+// import 'element-ui/lib/theme-green/index.css'  //浅绿色主题
+// import SIdentify from './components/Identify'
+import 'babel-polyfill'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+// Vue.component("SIdentify",SIdentify);
+Vue.use(ElementUI);
+
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+Vue.prototype.$http = window.axios
+
+// Vue.use(axios);
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app');
+
+
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
